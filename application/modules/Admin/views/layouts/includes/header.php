119fdf5b6d5131b2bf22aa49ad0e079c60d6e100
samuelw <samuel.wanjohi@nanyukiappfactory.onmicrosoft.com>
2019-03-15 11:02:09 +0300

    Fontawesome updated

--- application/modules/Admin/views/layouts/includes/header.php
+++ application/modules/Admin/views/layouts/includes/header.php
@@ -5,7 +5,7 @@
 	<?php echo $title; ?>
 </title>
 
- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/themes/fontawesome/css/all.css">
+ <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/fontawesome/css/all.css">
 
 <!-- Custom fonts for this template-->
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
