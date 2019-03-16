9dbc6bee3c34cc084bb8dd4879693f89329264e0
samuelw <samuel.wanjohi@nanyukiappfactory.onmicrosoft.com>
2019-03-16 11:30:50 +0300

    Indentation

--- application/modules/Admin/views/layouts/includes/header.php
+++ application/modules/Admin/views/layouts/includes/header.php
@@ -9,4 +9,5 @@
 
 <!-- Custom styles for this template-->
 <link href="<?php echo base_url(); ?>assets/themes/custom/styles.css" rel="stylesheet">
+
 <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.png" type="image/x-icon" />
